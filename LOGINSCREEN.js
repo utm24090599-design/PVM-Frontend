2298d4615f3bc0cd0986fac8a7cfa610f321c740
Naty11008 <utm24090757@utma.edu.mx>
2025-11-12 12:40:08 -0600

    Todo listo?

---
+++ LOGINSCREEN.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function LoginForm()
+{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async(event) => {
+    event.preventDefault();
+        try
+        {
+            const response = await axios.post('URL_BACKEND/api/login', {
+                email,
+        password,
+      });
+            console.log(response.data);
+            // Redirigir al usuario a la página principal después del login exitoso
+        }
+        catch (error)
+        {
+            setError(error.response.data.message);
+        }
+    }
+    ;
+
+    return (
+  
+      < form onSubmit ={ handleSubmit}>
+  
+        < h2 > Login </ h2 >
+  
+        < label >
+          Email:
+        < input
+          type = "email"
+          value ={ email}
+onChange ={ (event) => setEmail(event.target.value)}
+        />
+      </ label >
+      < br />
+      < label >
+        Contraseña:
+        < input
+          type = "password"
+          value ={ password}
+onChange ={ (event) => setPassword(event.target.value)}
+        />
+      </ label >
+      < br />
+      < button type = "submit" > Iniciar sesión </ button >
+      { error && < p style ={ { color: 'red' } }>{ error}</ p >}
+    </ form >
+  );
+}
+
+export default LoginForm;
