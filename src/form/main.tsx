5c76a70485805755ee32b050c8a522087633f562
SukheeM <cesarmd3742y@gmail.com>
2025-11-12 18:05:29 -0600

    Unfinished

--- src/form/main.tsx
+++ src/form/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FormApp from "./form/formapp";
-import "./form/index.css";
+import FormApp from "./formapp";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
