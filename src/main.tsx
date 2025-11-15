98c7c664c2cd205bb384c1174f518378230cffbd
SukheeM <cesarmd3742y@gmail.com>
2025-11-15 13:03:12 -0600

    Actualizado

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 // import './index.css'
-import App from './App.tsx'
+import FormApp from './form/formapp.tsx'
 import { AuthProvider } from './auth/AuthProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <AuthProvider>
         <BrowserRouter>
-          <App />
+          <FormApp />
         </BrowserRouter>
       </AuthProvider>
   </StrictMode>,
