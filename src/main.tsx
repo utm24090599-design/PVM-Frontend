f351c9079abfb5e80381a4df6540bf228b14fc00
Cesar Eduardo Castañeda López <utm24090599@utma.edu.mx>
2025-11-18 14:50:29 -0600

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// import './index.css'
-import FormApp from './form/formapp.tsx'
 import { AuthProvider } from './auth/AuthProvider.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <AuthProvider>
         <BrowserRouter>
-          <FormApp />
+          <App />
         </BrowserRouter>
       </AuthProvider>
   </StrictMode>,
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-// import Login from '../pages/TestLogin';
+import Login from '../pages/TestLogin';
 import LoginForm from '../components/LoginScreen';
 import Dashboard from '../pages/TestDashboard';
 // import NotFound from '../pages/NotFound';
 import ProtectedRoute from '../auth/ProtectedRoute';
 import Form from '../form/form';
+import TestConnection from '../components/Testconection';
 
 export default function AppRouter() {
   return (
@@ -20,6 +21,9 @@ export default function AppRouter() {
         }
       />
       {/* <Route path="*" element={<NotFound />} /> */}
+      {/* Test Routes for devs */}
+      <Route path="/TestConn" element={<TestConnection />} />
+      <Route path="/TestLogin" element={<Login />} />
     </Routes>
   );
 }
