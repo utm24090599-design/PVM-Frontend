eb445e8392c499a6275d79b920e1daac2a23cff3
Naty11008 <utm24090757@utma.edu.mx>
2025-11-21 13:20:00 -0600

    Modification7.1

--- src/Components/LoginScreen.tsx
+++ src/Components/LoginScreen.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
 import EmailInput from './inputs/EmailInput';
-import NameInput from './inputs/NameInput';
 import PasswordInput from './inputs/PasswordInput';
 
 function LoginForm()
