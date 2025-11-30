0287a91eab9419e41e112825a47fd77e4efbad84
Cesar Eduardo Castañeda López <utm24090599@utma.edu.mx>
2025-11-30 14:24:20 -0600

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css'
 import AppRouter from './routes/AppRoutes'
-import { FeedbackProvider } from './providers/FeedbackProvider'
-import { CartProvider } from './Components/CartContext'
-
 
 function App() {
   return (
